e1241773c6501cacb8125c00057951cf5fa1d685
AlexKnauth <alexander@knauth.org>
2014-07-29 11:42:47 -0400

    add one in a begin-for-syntax

--- experiment-with-hygenic-reader-extensions-2/try-it.rkt
+++ experiment-with-hygenic-reader-extensions-2/try-it.rkt
@@ -1,5 +1,16 @@
 #lang experiment-with-hygenic-reader-extensions-2
-(require (only-in "lang/reader.rkt"))
+(require (only-in "lang/reader.rkt")
+         (for-syntax racket/base
+                     (only-in "lang/reader.rkt")
+                     (for-syntax racket/base racket/syntax)))
 (car '$)
 (let ([lambda "whatever"])
   $)
+(begin-for-syntax
+  (displayln (car '$))
+  (define-syntax (shift-phase-level stx)
+    (syntax-case stx ()
+      [(shift-phase-level stx-expr shift)
+       (syntax-shift-phase-level #'stx-expr (syntax-local-eval #'shift))]))
+  (displayln (shift-phase-level $ 1))
+  )
